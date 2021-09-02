770da59ea89416e182bf1bbfb68ecc01faeefe68
nitingangyan <nitingangyan@gmail.com>
2021-09-02 23:51:07 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { Assessment } from './components/assessment';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Assessment from './components/assessment';
+import Leaderboard from './components/leaderboard';
+
 import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <Assessment />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Assessment</Link>
+          </li>
+          <li>
+            <Link to="/leaderboard">Leaderboard</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Assessment />
+          </Route>
+          <Route exact path="/leaderboard">
+            <Leaderboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
--- src/components/assessment.js
+++ src/components/assessment.js
@@ -1,16 +1,87 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const Assessment = () => {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
   const [questions, setQuestions] = useState([]);
-  const [currentQuestion, setCurrentQuestion] = useState({});
+  const [currentQuestion, setCurrentQuestion] = useState({
+    text: '',
+    options: ['A', 'B', 'C'],
+    answer: 1
+  });
   const [index, setIndex] = useState(0);
   var questionIndexes = [];
   while (questionIndexes.length < questions.length) {
     var r = Math.floor(Math.random() * questions.length);
     if (questionIndexes.indexOf(r) === -1) questionIndexes.push(r);
   }
+  const [open, setOpen] = React.useState(false);
 
-  return <div>Test</div>;
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Assessment
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary" />
+          <Typography variant="body2" component="p">
+            {currentQuestion.text}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" color="primary" onClick={handleClick}>
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          This is a success message!
+        </Alert>
+      </Snackbar>
+    </div>
+  );
 };
 
 export default Assessment;
