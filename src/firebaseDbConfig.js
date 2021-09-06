9f8ee84c8111d9f69b444f6275f92cf9c80f81af
nitingangyan <nitingangyan@gmail.com>
2021-09-06 13:27:07 +0530

    2 files modified

--- src/firebaseDbConfig.js
+++ src/firebaseDbConfig.js
@@ -1,25 +1,27 @@
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app';
-import * as firebase from 'firebase';
+import { initializeApp } from 'firebase/app';
+import 'firebase/database';
+// import * as firebase from 'firebase';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDImT0_Cclbp7-By6q8MP7LhqzocRtXS6I',
-  authDomain: 'assessment-e61cb.firebaseapp.com',
-  databaseURL: 'https://assessment-e61cb-default-rtdb.firebaseio.com',
-  projectId: 'assessment-e61cb',
-  storageBucket: 'assessment-e61cb.appspot.com',
-  messagingSenderId: '338146276572',
-  appId: '1:338146276572:web:80e0649f4afe722513f019'
+  apiKey: 'AIzaSyBoHwTZ7jnO56imcGqvDd26CfdsFZukSDM',
+  authDomain: 'leaderboard-41287.firebaseapp.com',
+  databaseURL: 'https://leaderboard-41287-default-rtdb.firebaseio.com',
+  projectId: 'leaderboard-41287',
+  storageBucket: 'leaderboard-41287.appspot.com',
+  messagingSenderId: '939570793321',
+  appId: '1:939570793321:web:afef831d19d188d0f78bf8'
 };
 
 // Initialize Firebase
-// const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+// export default firebaseApp;
 // console.log(firebaseApp);
-var fireDb = firebase.initializeApp(firebaseConfig);
+// var fireDb = firebase.initializeApp(firebaseConfig);
 
-export default fireDb.database().ref();
+// export default firebaseApp.database().ref();
 
 // export default firebaseApp.database().ref();
